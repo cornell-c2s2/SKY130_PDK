4c87fd3bac34ce5980494048e6b18ac7c9d843b4
David Bertuch <90874635+DavidDavidDavidDavidDavidDavidDavid@users.noreply.github.com>
2024-09-25 00:47:52 -0400

    Update setup_analog.sh

--- setup_analog.sh
+++ setup_analog.sh
@@ -9,5 +9,6 @@ unset OA_HOME
 #
 export CDS_Netlisting_Mode=Analog
 # SKY130 Specific Pegasus
+export OA_HOME=/opt/cadence/IC231/oa_v22.61.015
 export PEGASUS_DRC=~/SKY130_PDK/sky130_release/Sky130_DRC
 export PEGASUS_LVS=~/SKY130_PDK/sky130_release/Sky130_LVS
